730592e9b609d14a37f95bd9ee05015c6a7fa040
sazk07 <68821506+sazk07@users.noreply.github.com>
2020-07-26 22:26:09 +0500

    Add files via upload

---
+++ conditional_programming.py
@@ -0,0 +1,19 @@
+x=5
+if x>2:
+    print('Bigger than 2')
+    print('Still bigger')
+print ('done with 2')
+
+for i in range (5) :
+    print (i) 
+    if i > 2 :
+        print ('Bigger than 2')
+    print ('Done with i',i )
+print ('All done')
+
+y = 4
+if y >2:
+    print('Bigger')
+else:
+    print ('Smaller')
+print ('All done')
